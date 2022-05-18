46a4cd78197e97f62c8b2a9e9ee90f49b1cdad53
Jose L. Ulloa <jose.ulloa@isandex.com>
2022-05-18 01:09:36 -0400

    Added a webcam view

--- eyes.js
+++ eyes.js
@@ -5,16 +5,50 @@ document.onmousemove = (event) => {
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
   for (let i=0; i<balls.length; i++) {
-    console.log(balls[i]);
     balls[i].style.left = x;
     balls[i].style.top = y;
     balls[i].transform = 'translate(-' + x + ',-' + y + ')';
   }
-  // balls[0].style.left = x;
-  // balls[0].style.top = y;
-  // balls[0].transform = 'translate(-' + x + ',-' + y + ')';
-
-  // balls[1].style.left = x;
-  // balls[1].style.top = y;
-  // balls[1].transform = 'translate(-' + x + ',-' + y + ')';
 };
+
+
+// JU added:
+const eyesFrame = document.getElementById('video_overlays');
+// Definimos las constantes que vamos a utilizar
+const videoFrame = document.getElementById('video_frame');
+const canvasFrame = document.getElementById('canvas_frame');
+const snapFrame = document.getElementById("snap_frame");
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+// Definimos tamaño del video y si queremos audio o no
+const constraints = {
+    audio: true,
+    video: {
+        width: 720, height: 405
+    }
+};
+
+// Comprobamos acceso a la Webcam
+async function init() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+    } catch (e) {
+        errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+    }
+}
+
+// En caso de que el acceso sea correcto, cargamos la webcam
+async function handleSuccess(stream) {
+    window.stream = stream;
+    videoFrame.srcObject = stream;
+}
+
+// Iniciamos JS
+init();
+
+// Hacemos captura de pantalla al hacer click
+var context = canvasFrame.getContext('2d');
+snapFrame.addEventListener("click", function() {
+    context.drawImage(videoFrame, 0, 0, 320, 140);
+});
