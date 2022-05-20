ea156cf98a7889b66722e19cec10283129592d19
Jose L. Ulloa <jose.ulloa@isandex.com>
2022-05-20 00:51:48 -0400

    Core functionality is ok, resize and match the cam

--- assets/js/eyes.js
+++ assets/js/eyes.js
@@ -1,19 +1,36 @@
 const balls = document.getElementsByClassName('ball');
+const rtop = document.getElementById('area_frame').offsetTop;
+const rleft = document.getElementById('area_frame').offsetLeft;
+const height = document.getElementById('video_frame').offsetHeight;
+const width = document.getElementById('video_frame').offsetWidth;
+// const height = window.innerHeight;
+// const width = window.innerWidth;
 
 document.onmousemove = (event) => {
-  const x = (event.clientX * 100) / window.innerWidth + '%';
-  const y = (event.clientY * 100) / window.innerHeight + '%';
-
-  for (let i=0; i<balls.length; i++) {
-    balls[i].style.left = x;
-    balls[i].style.top = y;
-    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
-  }
+    const x = Math.min(...[(event.clientX * 100) / width, 100]) + '%'; 
+    const y = Math.min(...[(event.clientY * 100) / height, 100]) + '%';
+    console.log('X: '+ event.clientX + '(L:' + rleft + ', W:' + (rleft+width) + ')');
+    console.log('Y: '+ event.clientY + '(T:' + rtop + ', H:' + (rtop+height) + ')');
+    
+    for (let i=0; i<balls.length; i++) {
+        balls[i].style.left = x;
+        balls[i].style.top = y;
+        balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+    }
 };
 
-
 // JU added:
-const eyesFrame = document.getElementById('video_overlays');
+var slider = document.querySelector('#sizeRange') // getElementById("slidecontainer");
+var leye = document.getElementById('leye');
+var reye = document.getElementById('reye');
+slider.addEventListener('input', e => {
+    leye.style.width = e.target.value/4 + '%';    
+    reye.style.width = e.target.value/4 + '%';
+    for (i=0; i<balls.length; i++) {
+        balls[i].style.width = e.target.value/2 + '%';
+        balls[i].style.height = e.target.value + '%';
+    } 
+})
 // Definimos las constantes que vamos a utilizar
 const videoFrame = document.getElementById('video_frame');
 const canvasFrame = document.getElementById('canvas_frame');
@@ -48,7 +65,7 @@ async function handleSuccess(stream) {
 init();
 
 // Hacemos captura de pantalla al hacer click
-var context = canvasFrame.getContext('2d');
-snapFrame.addEventListener("click", function() {
-    context.drawImage(videoFrame, 0, 0, 320, 140);
-});
+// var context = canvasFrame.getContext('2d');
+// snapFrame.addEventListener("click", function() {
+//     context.drawImage(videoFrame, 0, 0, 320, 140);
+// });
