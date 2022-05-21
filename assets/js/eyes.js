449a682d7dbe4159868cfe16a23af86dbbe5cb82
Jose L. Ulloa <jose.ulloa@isandex.com>
2022-05-21 03:08:44 -0400

    ready to be published for peer review

--- assets/js/eyes.js
+++ assets/js/eyes.js
@@ -1,51 +1,61 @@
 const balls = document.getElementsByClassName('ball');
-const rtop = document.getElementById('area_frame').offsetTop;
-const rleft = document.getElementById('area_frame').offsetLeft;
-const height = document.getElementById('video_frame').offsetHeight;
-const width = document.getElementById('video_frame').offsetWidth;
-// const height = window.innerHeight;
-// const width = window.innerWidth;
+
+const area_frame = document.getElementById('area_frame');
+const area_rect = area_frame.getBoundingClientRect();
+
+const rtop = area_rect.top;
+const rleft = area_rect.left;
+const height = document.getElementById('video_frame').clientHeight;
+const width = document.getElementById('video_frame').clientWidth;
+
+console.log('Rtop: '+rtop);
+console.log('Rleft: '+rleft);
+console.log('Height: '+height);
+console.log('Width: '+width);
 
 document.onmousemove = (event) => {
-    const x = Math.min(...[(event.clientX * 100) / width, 100]) + '%'; 
-    const y = Math.min(...[(event.clientY * 100) / height, 100]) + '%';
-    // console.log('X: '+ event.clientX + '(L:' + rleft + ', W:' + (rleft+width) + ')');
-    // console.log('Y: '+ event.clientY + '(T:' + rtop + ', H:' + (rtop+height) + ')');
+    let xpos = event.clientX - rleft;
+    let ypos = event.clientY - rtop;
+    
+    let x = (100 * Math.min(...[Math.max(...[0, xpos]), width]) / width) ;
+    let y = (100 * Math.min(...[Math.max(...[0, ypos]), height]) / height) - 50;
+
+    console.log('X: '+ x + '(L:' + (x*width/100) + ', W:' + (rleft+width) + ')');
+    console.log('Y: '+ y + '(T:' + (y*height/100) + ', H:' + (rtop+height) + ')');
     
     for (let i=0; i<balls.length; i++) {
-        balls[i].style.left = x;
-        balls[i].style.top = y;
+        balls[i].style.left = (2*x)  + 'px';
+        balls[i].style.top = (y) + 'px';
         balls[i].transform = 'translate(-' + x + ',-' + y + ')';
     }
 };
 
-// JU added:
+// Slider to resize:
 var slider = document.querySelector('#sizeRange') // getElementById("slidecontainer");
 var leye = document.getElementById('leye');
 var reye = document.getElementById('reye');
 slider.addEventListener('input', e => {
-    leye.style.width = e.target.value/4 + '%';    
-    reye.style.width = e.target.value/4 + '%';
+    leye.style.width = (e.target.value * 40 / 100) + '%';    
+    reye.style.width = (e.target.value * 40 / 100) + '%';
     for (i=0; i<balls.length; i++) {
-        balls[i].style.width = e.target.value/2 + '%';
+        balls[i].style.width = (e.target.value * 30 / 100) + '%';
         balls[i].style.height = e.target.value + '%';
     } 
 })
-// Definimos las constantes que vamos a utilizar
+// Elements from the DOM
 const videoFrame = document.getElementById('video_frame');
 const canvasFrame = document.getElementById('canvas_frame');
 const snapFrame = document.getElementById("snap_frame");
 const errorMsgElement = document.querySelector('span#errorMsg');
 
-// Definimos tamaño del video y si queremos audio o no
+// Video size
 const constraints = {
-    audio: true,
     video: {
-        width: 720, height: 405
+        width: 720
     }
 };
 
-// Comprobamos acceso a la Webcam
+// Check webcam access
 async function start_cam() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -55,19 +65,18 @@ async function start_cam() {
     }
 }
 
-// En caso de que el acceso sea correcto, cargamos la webcam
+// If webcam ok, then start streaming:
 async function handleSuccess(stream) {
     window.stream = stream;
     videoFrame.srcObject = stream;
 }
 
 // Enable stop button
+async function stop_cam() {
+    let tracks = window.stream.getTracks();
 
-// Iniciamos JS
-// init();
-
-// Hacemos captura de pantalla al hacer click
-// var context = canvasFrame.getContext('2d');
-// snapFrame.addEventListener("click", function() {
-//     context.drawImage(videoFrame, 0, 0, 320, 140);
-// });
+    tracks.forEach(function (track) {
+        track.stop();
+        track.srcObject = null;
+    });
+}
